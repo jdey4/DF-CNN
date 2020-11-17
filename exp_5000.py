07f0afa7388ae26821922506fdf3afc77f8f32f7
Ubuntu <ubuntu@ip-172-31-8-157.ec2.internal>
2020-11-17 16:03:26 +0000

    5000 sample run

--- exp_5000.py
+++ exp_5000.py
@@ -121,7 +121,7 @@ if not os.path.exists(data_folder):
 
 num_points_per_task = 5000
 slot_fold = range(1)
-shift_fold = range(1,2,1)
+shift_fold = range(2,3,1)
 algs = range(2)
 
 for shift in shift_fold:
